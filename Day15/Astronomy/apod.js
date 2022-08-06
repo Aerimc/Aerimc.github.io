57ba8cefb765a391c46085dbd8bfbcae01afb9fe
SimpsonTew <smntewelde70@gmail.com>
2022-08-06 04:02:58 -0500

    Day15 update

--- Day15/Astronomy/apod.js
+++ Day15/Astronomy/apod.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
   $("#view_button").click(getPicture);
-  console.log('apod');
+  console.log("apod");
 });
 function getPicture() {
   $.ajax({
     url: "https://api.nasa.gov/planetary/apod",
     type: "GET",
-    data: { api_key: "gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s", date: $("#date").val() },
+    data: {
+      api_key: "gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s",
+      date: $("#date").val(),
+    },
     dataType: "json",
     success: showPicture,
     error: noPicture,
   });
-console.log('getpic');
+  console.log("getpic");
 }
 function showPicture(data) {
   $("#pic").attr("src", data.url);
