df0a36bd6906eb8ca851c40321d6257c528a0069
SimpsonTew <smntewelde70@gmail.com>
2022-08-09 16:47:15 -0500

    Final Project

---
+++ Online English Dictionary - Final Project/dictionary.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var search = require("./word");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+// Serving the html for homepage request
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dict.html"));
+});
+
+// Searving the results page when lookup is clicked
+app.post("/search", function (req, res) {
+  search.wordMeaning(res, req.body.searchTerm);
+});
+
+var server = app.listen(8000, function () {
+  console.log("Server is running on port 8000...");
+});
